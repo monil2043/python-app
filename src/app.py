474126411c5b8c88c80e70f3615304ed728aee93
Monil <monil.ranka@gmail.com>
2025-09-10 09:49:05 +0530

    first push

--- src/app.py
+++ src/app.py
@@ -1,2 +1,11 @@
 def greet(name="world"):
     return f"hello, {name}!"
+
+def lambda_handler(event, context):
+    # If name is passed via API Gateway or test event, fetch it
+    name = event.get("name", "world")
+    message = greet(name)
+    return {
+        "statusCode": 200,
+        "body": message
+    }
